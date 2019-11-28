f1adf63262bae9ed21002f38a519544c481a3a1a
Christina Li <cli50@stevens.edu>
2019-11-27 17:53:29 -0800

    sensor was basically given

---
+++ hw5/sensor.erl
@@ -0,0 +1,13 @@
+-module(sensor).
+-compile(export_all).
+-author("Christina Li and Brenden Brusberg").
+
+sensor(ID, Watcher) ->
+  Measurement = rand:uniform(11),
+  if(Measurement == 11) ->
+    exit("anomalous_reading");
+    true ->
+      Watcher!{self(),{ID, Measurement}}
+  end,
+  Sleep_time = rand:uniform(10000),
+  timer:sleep(Sleep_time).
