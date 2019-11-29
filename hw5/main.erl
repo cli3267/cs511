34245e9b0c54bd9f0de9c78aa295e5d6b4d9c511
brusberg <brenbrus@gmail.com>
2019-11-28 23:10:10 -0500

    Start_loop

--- hw5/main.erl
+++ hw5/main.erl
@@ -3,14 +3,26 @@
 -author("Christina Li and Brenden Brusberg").
 
 setup_loop(N, Num_watchers) ->
-  todo.
+    setup_loop(N, Num_watchers, 0).
 
+setup_loop(N, Num_watchers, Count) ->
+    case Num_watchers of
+	Num when Num > 1 ->
+	    N_Start = Count * 10,
+	    N_End = min(N-1, (Count*10)+9),
+	    spawn(watcher, watcher, [N_Start, N_End]),
+	    setup_loop(N, Num_watchers-1, Count+1);
+	Num ->
+	    N_Start = Count * 10,
+	    N_End = min(N-1, (Count*10)+9),
+	    spawn(watcher, watcher, [N_Start, N_End])
+    end.
 
 start () ->
-  {ok , [ N ]} = io:fread("enter  number  of sensors > ", "~d"),
-  if N =< 1 ->
-    io:fwrite("setup: range  must be at  least  2~n", []);
-  true  ->
-    Num_watchers = 1 + (N div  10),
-    setup_loop(N, Num_watchers)
-  end.
+    {ok , [ N ]} = io:fread("enter  number  of sensors > ", "~d"),
+    if N =< 1 ->
+	    io:fwrite("setup: range  must be at  least  2~n", []);
+       true  ->
+	    Num_watchers = 1 + (N div 10),
+	    setup_loop(N, Num_watchers)
+    end.
--- hw5/watcher.erl
+++ hw5/watcher.erl
@@ -1,20 +1,30 @@
 -module(watcher).
--compile(compile_all).
+-compile(export_all).
 -author("Christina Li and Brenden Brusberg").
 
 watcher(S) ->
-  io:fwrite("Watcher: ~w, Initial List of Sensors: ~w~n", [self(), S]),
-  receive 
-    {"DOWN", _, process, Pid2, {ID, Reason}} ->
-      io:fwrite("Watcher: ~w, Termination Reason: ~w, Sensor: ~w~n", [self(), Reason, ID]),
-      RemovedSensor = lists:delete({ID, Pid2}, S),
-      { Pid, _ } = spawn_monitor(sensor, sensor, [ID, self()]),
-      UpdatedSensor = lists:append([{ID, Pid}], RemovedSensor),
-      io:fwrite("Watcher: ~w, Upated Sensors: ~w~n", [self(), UpdatedSensor]),
-      watcher(UpdatedSensor);
-    {ID, Measurement} ->
-      io:fwrite("Sensor: ~w, Measurement: ~w~n", [ID, Measurement]),
-      watcher(S)
-  end.
+    io:fwrite("Watcher: ~w, Initial List of Sensors: ~w~n", [self(), S]),
+    receive 
+	{"DOWN", _, process, Pid2, {ID, Reason}} ->
+	    io:fwrite("Watcher: ~w, Termination Reason: ~w, Sensor: ~w~n", [self(), Reason, ID]),
+	    RemovedSensor = lists:delete({ID, Pid2}, S),
+	    { Pid, _ } = spawn_monitor(sensor, sensor, [ID, self()]),
+	    UpdatedSensor = lists:append([{ID, Pid}], RemovedSensor),
+	    io:fwrite("Watcher: ~w, Upated Sensors: ~w~n", [self(), UpdatedSensor]),
+	    watcher(UpdatedSensor);
+	{ID, Measurement} ->
+	    io:fwrite("Sensor: ~w, Measurement: ~w~n", [ID, Measurement]),
+	    watcher(S)
+    end.
 
-
+watcher(N_Start, N_End) ->
+    io:fwrite("PID ~w~n", [self()]),
+    S = lists:apply(fun(ID)->
+			    {Pid, _} = spawn_monitor(sensor, sensor, [ID, self()]),
+			    {ID, Pid} end
+		   , lists:seq(N_Start,N_End)),
+    io:fwrite("WATCHER PID:~w, SENSOR LIST[{id,pid}]: ~w~n", [self(), S]), 
+    watcher(S).    
+    
+    
+			  
